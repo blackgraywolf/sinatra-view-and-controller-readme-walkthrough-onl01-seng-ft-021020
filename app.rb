b36686df93a39b8e08d570edb59f5091ef490377
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-28 20:19:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
- 
- post '/:reverse' do
-  original_string = params[:reverse]
-  @reversed_string = original_string.reverse
- 
-  # erb :reversed
-end
   
+post '/reverse' do
+  original_string = params["string"]
+  reversed_string = original_string.reverse
+ 
+  erb :reversed
+end
 
 
 get '/friends' do
