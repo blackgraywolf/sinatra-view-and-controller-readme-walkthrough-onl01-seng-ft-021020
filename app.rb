bc831a0aaecd3ac6e8093022f3ca37d344dbfe50
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-28 20:20:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,6 +9,16 @@ post '/reverse' do
   erb :reversed
 end
 
+get "/" do
+  @user = "Ian"
+ 
+  erb :index # @user will be defined as 'Ian' in the view
+end
+ 
+get "/profile" do
+  erb :profile # @user will be nil here
+end
+
 get '/friends' do
   @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
  
